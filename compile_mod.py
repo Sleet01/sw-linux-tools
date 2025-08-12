c9c1c343066c29e319e01199e1bed7677f2fac1a
sleet01 <sleet01@hotmail.com>
2025-08-11 23:44:30 -0700

    Replaced click with argparse

--- compile_mod.py
+++ compile_mod.py
@@ -1,13 +1,16 @@
 # /// script
 # requires-python = ">=3.11"
-# dependencies = ["rich", "click"]
+# dependencies = ["rich", "mypy", "typing"]
 # ///
 
-import click
-import subprocess
+import argparse
 import os
-from rich import print as rprint
+import subprocess
+import sys
+
+from typing import List
 from pathlib import Path
+from rich import print as rprint
 
 def set_env():
     env = os.environ.copy()
@@ -18,8 +21,27 @@ def set_env():
 MOD_COMPILER_CMD = ["/home/deck/.local/share/Steam/steamapps/common/Proton 9.0 (Beta)/proton", "run", "/home/deck/.steam/steam/steamapps/common/Stormworks/sdk/component_mod_compiler.com"]
 MESH_COMPILER_CMD = ["/home/deck/.local/share/Steam/steamapps/common/Proton 9.0 (Beta)/proton", "run", "/home/deck/.steam/steam/steamapps/common/Stormworks/sdk/mesh_compiler.com"]
 
+def parse_args(argv: List[str]) -> argparse.Namespace:
+    """                                                                                                      
+    Parse argv object for CLI arguments.                                                                     
+    """                                                                                                      
+                                                                                                             
+    desc = 'Compiles a SW mod from a definition file'
+    epi = f'Example: \n\t{sys.argv[0]} <definition file>'
+                                                                                                             
+    parser = argparse.ArgumentParser(description=desc, epilog=epi)
+    parser.add_argument(
+            "definition",
+            type=str,
+            required=True,
+            help='File containing mesh and mod definitions',
+    )
+                                                                                                             
+    args = parser.parse_args(argv)                                                                           
+                                                                                                             
+    return args
 
-def _compile_mesh(mesh_path: str):
+def _compile_mesh(mesh_path: str) -> None:
     rprint(f"Compiling mesh [bold]{mesh_path}[/bold]...")
     base_path = "/".join(mesh_path.split("/")[:-1])
     filename = mesh_path.split("/")[-1].replace(".dae", ".mesh")
@@ -27,7 +49,7 @@ def _compile_mesh(mesh_path: str):
     output_path = f"{base_path}/{filename}"
     rprint(f"Successfully compiled mesh: [bold]{output_path}[/bold]")
 
-def _compile_mod(definition: str, assets: tuple[str]):
+def _compile_mod(definition: str, assets: List[str]):
     rprint("Compiling mod...")
     result = subprocess.run(MOD_COMPILER_CMD + [definition] + [*assets], env=set_env(), capture_output=True)
     bin_path = definition.replace(".xml", ".bin")
@@ -44,26 +66,31 @@ def _compile_mod(definition: str, assets: tuple[str]):
     else:
         rprint("Failed to compile mod.")
 
-@click.command()
-@click.argument("definition", type=click.Path(exists=True))
-def compile(definition: str):
-    path = "/".join(definition.split("/")[:-1])
-    files = os.listdir(path)
-    xml_file = definition
+def compile(args: argparse.Namespace) -> int:
+    result: int = 0
+    try:
+        definition: str = args.definition
+        path = "/".join(definition.split("/")[:-1])
+        files = os.listdir(path)
+        xml_file = definition
 
-    lua_files = [f"{path}/{x}" for x in files if x.endswith(".lua")]
-    ogg_files = [f"{path}/{x}" for x in files if x.endswith(".ogg")]
-    dae_files = [f"{path}/{x}" for x in files if x.endswith(".dae")]
+        lua_files = [f"{path}/{x}" for x in files if x.endswith(".lua")]
+        ogg_files = [f"{path}/{x}" for x in files if x.endswith(".ogg")]
+        dae_files = [f"{path}/{x}" for x in files if x.endswith(".dae")]
 
-    # Compile meshes first
-    mesh_files = []
-    for dae_file in dae_files:
-        _compile_mesh(dae_file)
-        mesh_files.append(dae_file.replace('.dae', '.mesh'))
+        # Compile meshes first
+        mesh_files = []
+        for dae_file in dae_files:
+            _compile_mesh(dae_file)
+            mesh_files.append(dae_file.replace('.dae', '.mesh'))
 
-    # Compile the mod itself
-    asset_files = mesh_files + lua_files + ogg_files
-    _compile_mod(definition=definition, assets=asset_files)
+        # Compile the mod itself
+        asset_files = mesh_files + lua_files + ogg_files
+        _compile_mod(definition=definition, assets=asset_files)
+    except:
+        result = 1
+
+    return result
 
 if __name__ == '__main__':
-    compile()
+    sys.exit(compile(parse_args(sys.argv[1:])))
